8b4079e44d881ad43832e2e9b53db60b8289e0c3
Voxense <voxensesol@gmail.com>
2025-11-26 10:55:51 +0700

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        vox_bg: "#020714",
+        vox_neon: "#C0FF00"
+      }
+    }
+  },
+  plugins: []
+};
